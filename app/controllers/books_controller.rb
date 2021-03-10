13ee0f9c621c827ea971e65b261e4c8c9dea7f3d
pkoky <sscsk2224@gmail.com>
2021-03-10 06:29:14 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,6 +24,13 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
 
 
   private
