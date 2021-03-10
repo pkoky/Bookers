1e1ea43dfb82d4b76494c843068809de624cd69f
pkoky <sscsk2224@gmail.com>
2021-03-10 04:56:35 +0000

    [Add]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,25 +5,29 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to root_path
+    redirect_to book_path(book.id)
   end
-  
+
 
   def show
+    @book = Book.find(params[:id])
   end
-end
 
 
-private
-def book_params
-  params.require(:book).permit(:title, :body)
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
